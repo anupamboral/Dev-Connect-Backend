02079ce5f5a1071599834286b9352aef4b7fc595
anupamboral <anupamboral6889@gmail.com>
2025-11-22 14:49:33 +0530

    built the pagination feature in feed api

--- allLessonNotes.js
+++ allLessonNotes.js
@@ -1861,6 +1861,92 @@ const connectionRequest = await ConnectionRequest.find({
   $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
 }).select("fromUserId toUserId"); //!select() method if we have a object or arrays of objects and we don't need of the fields inside the object, then we use this select method which takes only one arg which is a string where we have to write on;y required field names separated with spaces, and it will return the object or objects with required fields mention in the args of the select method.
 
-//*  Then we will create a new Set() and assign it as a value of a constant named hiddenProfiles ,Remember a set always saves only unique values, no duplicate values, and then using the forEach method we will loop the all the connections we found and add then inside the set we will add all the fromUserIds and toUserIds, so it will included both the loggedInUser and also the user's who are connected(interested , ignored, accepted,rejected) , the we will write a query using the User model using .find() method. and inside this we will write the logic that except the hiddenUserFromFeed profiles and loggedInUser profile fetch all the other users profile. To write that we will use $and:[] operator to write and query, and $nin to write not in the array query and $ne to write not equal  to query, and also we will use Array.from() method to convert the  set to array., and finally save the response into a constant name visibleFeedUsers then send back response to the client.
+//*  Then we will create a new Set() and assign it as a value of a constant named hiddenProfiles ,Remember a set always saves only unique values, no duplicate values,like below,
+const hiddenUsersFromFeed = new Set();
 
-//* write further comments inside the api and in above comment with explanations with reference links.
+//*  and then using the forEach method we will loop the all the connections we found and add then inside the set we will add all the fromUserIds and toUserIds, so it will included both the loggedInUser and also the user's who are connected(interested , ignored, accepted,rejected) ,like below:-
+connectionRequests.forEach((connection) => {
+  hiddenUsersFromFeed.add(connection.fromUserId.toString());
+  hiddenUsersFromFeed.add(connection.toUserId.toString());
+});
+// * then we will write a query using the User model using .find() method. and inside this we will write the logic that except the hiddenUserFromFeed profiles and loggedInUser profile fetch all the other users profile. To write that we will use $and:[] operator to write and query, and $nin to write not in the array query and $ne to write not equal  to query, and also we will use Array.from() method to convert the hiddenUserFromFeed set to array., and finally save the response into a constant name visibleFeedUsers then send back response to the client.Like below:-
+const visibleFeedUsers = await User.find({
+  $and: [
+    { _id: { $nin: Array.from(hiddenUsersFromFeed) } },
+    { _id: { $ne: loggedInUser._id } },
+  ],
+}).select(USER_SAFE_DATA);
+//* $and operator ensures that , whatever condition written inside the array all of them gets fulfilled
+//* $nin means "not in the array" , so it will only return the results from db which are not included in the array(Array.from(hiddenUsersFromFeed)).
+//*$nin selects the documents where:
+//*the specified field value is not in the specified array or
+//*the specified field does not exist.
+//* syntax :- { fieldName: { $nin: [ <value1>, <value2> ... <valueN> ] } }
+//* so which ever doc include a field with any value present inside the array , it will not fetch that doc.that's why it represents "not in the array".
+// *Array.from() converts the set(hiddenUserFromFeed) into a array. in $nin we have to mention a array not a set so that's why we used this method.
+// * $ne selects the documents where the value of the specified field is not equal to passed value by us basically it represents "not Equal to". This includes documents that do not contain the specified field. so it will not return any doc which includes the field(here _id) with mentioned value(here loggedInUser._id).
+//*syntax:- { field: { $ne: value } }
+//*   Because of the first condition  { _id: { $nin: Array.from(hiddenUsersFromFeed) } }, it will not return any user included in the hidden list and also it will not return the profile doc of the loggedInUser because of the second condition   { _id: { $ne: loggedInUser._id } }.and then we are only selecting the required fields using select method so not all the fields will be there in the returned data from db like password or email as these are not required.
+//* So finally the api will look like below:-
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    //* getting the loggedInUser as we already saved it inside the userAuth while validating the token
+    const loggedInUser = req.user;
+    //*  getting the all the connection requests with the "interested , ignored,accepted, rejected statuses, then only selecting the fromUserId and toUserId using the select() method
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    //* creating a new Set() and assign it as a value of a constant named hiddenProfiles ,Remember a set always saves only unique values, no duplicate values
+    const hiddenUsersFromFeed = new Set();
+
+    //* using the forEach method we will loop the all the connections we found and add then inside the set we will add all the fromUserIds and toUserIds, so it will included both the loggedInUser and also the user's who are connected(interested , ignored, accepted,rejected)
+    connectionRequests.forEach((connection) => {
+      hiddenUsersFromFeed.add(connection.fromUserId.toString());
+      hiddenUsersFromFeed.add(connection.toUserId.toString());
+    });
+
+    // * doing a query to the database using the User model using .find() method. and inside this we will write the logic that except the hiddenUserFromFeed profiles and loggedInUser profile fetch all the other users profile. To write that we will use $and:[] operator to write and query, and $nin to write not in the array query and $ne to write not equal  to query, and also we will use Array.from() method to convert the hiddenUserFromFeed set to array., and finally save the response into a constant name visibleFeedUsers then send back response to the client.
+    const visibleFeedUsers = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddenUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA); //* $nin means not array , so it will only return the results from db which are not included in the array, so only the ids that in our hiddenUserFromFeed . Array.from() converts the set into a array. $ne selects the documents where the value of the specified field is not equal to passed value by us basically it represents not Equal to. This includes documents that do not contain the specified field. so it will not return any users who is included in the hidden array list  because of the first condition and also it will not return the profile doc of the loggedInUser because of the second condition.and then we are only selecting the required fields using select method not all the fields from the returned dat from db like password or email as these are not required.
+    // console.log(visibleFeedUsers);
+    //* sending back response
+    res.json({
+      data: visibleFeedUsers,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("something went wrong:- " + err.message);
+  }
+});
+
+//! Pagination
+//* suppose We have millions of users And a new user signs in So in the feed Will he see all of the users At once No that should not happen at once in the feed There should Only 10 users And when He will go to the next page You will see Next 10 new users So from 11 to 20, Then again when he goes to the third page he should see the users from 21 to 30 So basically we are trying to implement The pagination feature So to implement the pagination feature We can use the query params, like:- /feed?page=1&limit=10 , in the url, so to Implement this feature we need to know two important functions Of Mongodb First is skip() and second one is limit(), so skip() Function represents how many documents It has to skip, so if we pass 10 as arg in skip(10) then , it will skip first 10 docs if we pass 20 the it will skip first 20 docs , and give docs from 21 , and limit() function the number of how many docs it should give, so if we pass 10 in limit(10) then it will give total 10 docs, so if we pass skip(10)&limit(10) that means it will give first 10 docs then give us 10 docs so 11 to 20 num doc.like below:-
+//* /feed?page=1&limit=10 => 1 - 10 => skip(0)&limit(10)
+//* /feed?page=2&limit=10 => 11 - 20 => skip(10)&limit(10)
+//* /feed?page=1&limit=10 => 21 - 30 => skip(20)&limit(10)
+//* so humans will understand left side query params , so they can search for page 2,  but mongodb will not understand that , mongo will understand skip(10)&limit(10), so that's why learning this skip() and limit() is understand.
+//*so inside the feed api at top portion we will we will doi it like below:-
+//* implementation of pagination
+//* query params are optional so we don't need to change anything in the api url.
+const page = parseInt(req.query.page) || 1; //* parseInt will convert the string into integer , and if nothing has come oe some wrong thing is coming through query then set it to 1 by default.
+let limit = parseInt(req.query.limit) || 10; //* parseInt will convert the string into integer , and if nothing has come oe some wrong thing is coming through query then set it to 10 by default.
+//* if user pass 100000 as limit then while doing the query our db will haag while doing a so much big query so we should always restrict this limit we will set it to 50 max.
+limit = limit > 50 ? 50 : limit;
+
+const skip = (page - 1) * limit; //* suppose someone is searching page 3 then (3-1)*10=20 so it will skip first 20 pages.
+
+//* then go where we are finding the users from the user collection. and at last we will chain this methods skip and limit method  like below:-
+const visibleFeedUserss = await User.find({
+  $and: [
+    { _id: { $nin: Array.from(hiddenUsersFromFeed) } },
+    { _id: { $ne: loggedInUser._id } },
+  ],
+})
+  .select(USER_SAFE_DATA)
+  .skip(skip)
+  .limit(limit); //*skip method will skip the number of pages what ever passed inside it, and limit method only give the number of results we pass inside it, like we the user pass page 2 then (2-1)*10=10 , so it will skip first ten pages and if the limit is passed 10 then after skipping first 10 pages because of the skip method it will return 10 docs it will show docs from 11-20.
--- src/routes/user.js
+++ src/routes/user.js
@@ -83,27 +83,43 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
+    //* implementation of pagination
+    const page = parseInt(req.query.page) || 1; //* parseInt will convert the string into integer , and if nothing has come oe some wrong thing is coming through query then set it to 1 by default.
+    let limit = parseInt(req.query.limit) || 10; //* parseInt will convert the string into integer , and if nothing has come oe some wrong thing is coming through query then set it to 10 by default.
+    //* if user pass 100000 as limit then while doing the query our db will hang while doing a so much big query so we should always restrict this limit we will set it to 50 max.
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit; //* suppose someone is searching page 3 then (3-1)*10=20 so it will skip first 20 pages.
     //* in the feed of the loggedInUser we should not show any other connection with interested,ignored,accepted,rejected status, and also not show the loggedInUser to himself, we should only show the new people in the feed of anu loggedInUser. So we will find all user's where either the loggedInUser's Id is same as fromUserId or toUserId from the ConnectionRequest Model and the we will use select("field1 field2") method to only save the required fields into the connection Requests. So we can find all the friends(accepted status, who sent or received and accepted) , an also who rejected or ignored loggedInUser's profile. Then we will create a new Set() and assign it as a value of a constant named hiddenProfiles ,Remember a set always saves only unique values, no duplicate values, and then using the forEach method we will loop the all the connections we found and add then inside the set we will add all the fromUserIds and toUserIds, so it will included both the loggedInUser and also the user's who are connected(interested , ignored, accepted,rejected) , the we will write a query using the User model using .find() method. and inside this we will write the logic that except the hiddenUserFromFeed profiles and loggedInUser profile fetch all the other users profile. To write that we will use $and:[] operator to write and query, and $nin to write not in the array query and $ne to write not equal  to query, and also we will use Array.from() method to convert the  set to array., and finally save the response into a constant name visibleFeedUsers then send back response to the client.
 
+    //* getting the loggedInUser as we already saved it inside the userAuth while validating the token
     const loggedInUser = req.user;
-
+    //*  getting the all the connection requests with the "interested , ignored,accepted, rejected statuses, then only selecting the fromUserId and toUserId using the select() method
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
 
+    //* creating a new Set() and assign it as a value of a constant named hiddenProfiles ,Remember a set always saves only unique values, no duplicate values
     const hiddenUsersFromFeed = new Set();
+
+    //* using the forEach method we will loop the all the connections we found and add then inside the set we will add all the fromUserIds and toUserIds, so it will included both the loggedInUser and also the user's who are connected(interested , ignored, accepted,rejected)
     connectionRequests.forEach((connection) => {
       hiddenUsersFromFeed.add(connection.fromUserId.toString());
       hiddenUsersFromFeed.add(connection.toUserId.toString());
     });
 
+    // * doing a query to the database using the User model using .find() method. and inside this we will write the logic that except the hiddenUserFromFeed profiles and loggedInUser profile fetch all the other users profile. To write that we will use $and:[] operator to write and query, and $nin to write not in the array query and $ne to write not equal  to query, and also we will use Array.from() method to convert the hiddenUserFromFeed set to array., and finally save the response into a constant name visibleFeedUsers then send back response to the client.
     const visibleFeedUsers = await User.find({
       $and: [
         { _id: { $nin: Array.from(hiddenUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA); //* $nin means not array , so it will only return the results from db which are not included in the array, so only the ids that in our hiddenUserFromFeed . Array.from() converts the set into a array. $ne selects the documents where the value of the specified field is not equal to passed value by us basically it represents not Equal to. This includes documents that do not contain the specified field. so it will not return any users who is included in the hidden array list  because of the first condition and also it will not return the profile doc of the loggedInUser because of the second condition.and then we are only selecting the required fields using select method not all the fields from the returned dat from db like password or email as these are not required.
-    console.log(visibleFeedUsers);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit); //* $nin means not array , so it will only return the results from db which are not included in the array, so only the ids that in our hiddenUserFromFeed . Array.from() converts the set into a array. $ne selects the documents where the value of the specified field is not equal to passed value by us basically it represents not Equal to. This includes documents that do not contain the specified field. so it will not return any users who is included in the hidden array list  because of the first condition and also it will not return the profile doc of the loggedInUser because of the second condition.and then we are only selecting the required fields using select method not all the fields from the returned dat from db like password or email as these are not required. skip method will skip the number of pages what ever passed inside it, and limit method only give the number of results we pass inside it, like we the user pass page 2 then (2-1)*10=10 , so it will skip first ten pages and if the limit is passed 10 then after skipping first 10 pages because of the skip method it will return 10 docs it will show docs from 11-20
+    // console.log(visibleFeedUsers);
+    //* sending back response
     res.json({
       data: visibleFeedUsers,
     });
