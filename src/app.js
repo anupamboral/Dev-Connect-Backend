99bb6f15962b32363f6f3b2d8cbcfc005ef4be43
anupamboral <anupamboral6889@gmail.com>
2025-12-14 11:42:12 +0530

    vercel  config file added

--- src/app.js
+++ src/app.js
@@ -17,8 +17,6 @@ const profileRouter = require("./routes/profile");
 const connectionRequestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
-
-
 //! importance of express.json() middleware.
 //* so to convert the json to js object , we will use a middleware given to us by express.js. and that's express.json(). this middleware can take the json from the req.body then convert that to js object then again put that inside the req.body and we can then easily access it form the request body.
 //* to use it we can just put it inside app.use(express.json())) , like we cerate middlewares, as we know app.use() work for all methods when we specify any path as first arg, but if we don't even specify any path then it will work for all requests with any path . so that's why we are using it without any path so it can work for any api. like this :- app.use(express.json()))
@@ -45,11 +43,14 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", connectionRequestRouter);
 app.use("/", userRouter);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express on Vercel!" });
+});
 
 connectDb()
   .then(() => {
     console.log("successfully connected to the database cluster");
-    app.listen(process.env.PORT, () => {
+    app.listen(3000, () => {
       console.log("server is listening successfully on port 3000");
     }); //* using the listen method we listening to the incoming requests on port number 3000, the first parameter of this listen method is the port number , now there is second parameter which is a callback function, and this will be called when our server is up and running.
   })
